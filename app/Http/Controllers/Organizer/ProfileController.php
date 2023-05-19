74055f80a1ab2af8cdeab71f37c706fdd821c939
Björn End <bjoern@endmail.de>
2023-05-20 00:19:37 +0200

    Diverse Anpassungen

--- app/Http/Controllers/Organizer/ProfileController.php
+++ app/Http/Controllers/Organizer/ProfileController.php
@@ -20,6 +20,7 @@ class ProfileController extends Controller
     {
         return view('organizer.profile.edit', [
             'user' => $request->user(),
+            'organizer' => $request->user()->organizer,
         ]);
     }
 
